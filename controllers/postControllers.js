2ba5f5087ecad91c5e250f67e45db9537dda1ad0
Elina Saarelainen <elina.t.saarelainen@gmail.com>
2023-11-08 16:03:34 +0200

    luokka Post ja postControllers päivitetty

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,11 +1,35 @@
+const Post = require("../models/Post");
+
 exports.getAllPosts = async (req, res, next) => {
-  res.send("Get all posts route");
+  try {
+    const [posts, _] = await Post.findAll();
+
+    res.status(200).json({ count: posts.length, posts });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
 exports.createNewPost = async (req, res, next) => {
-  res.send("Create new post route");
+  try {
+    let { title, body } = req.body;
+    let post = new Post(title, body);
+
+    post = await post.save();
+
+    res.status(201).json({ message: "Post created" });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
 exports.getPostById = async (req, res, next) => {
-  res.send("Get post by ID route");
+  try {
+    let postId = req.params.id;
+    let [post, _] = await Post.findById(postId);
+
+    res.status(200).json({ post: post[0] });
+  } catch (error) {}
 };
---
+++ models/Post.js
@@ -0,0 +1,48 @@
+const db = require("../config/db");
+
+class Post {
+  constructor(title, body) {
+    this.title = title;
+    this.body = body;
+  }
+
+  async save() {
+    let d = new Date();
+    let yyyy = d.getFullYear();
+    let mm = d.getMonth() + 1;
+    let dd = d.getDate();
+
+    let createAtDate = `${yyyy}-${mm}-${dd}`;
+
+    let sql = `
+INSERT INTO posts(
+    title,
+    body,
+    created_at
+)
+VALUES(
+    '${this.title}',
+    '${this.body}',
+    '${createAtDate}'
+)
+`;
+
+    const [newPost, _] = await db.execute(sql);
+
+    return newPost;
+  }
+
+  static findAll() {
+    let sql = "SELECT * FROM posts;";
+
+    return db.execute(sql);
+  }
+
+  static findById(id) {
+    let sql = `SELECT * FROM posts WHERE id = ${id}`;
+
+    return db.execute(sql);
+  }
+}
+
+module.exports = Post;
